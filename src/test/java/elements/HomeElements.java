8dea869b0e1f7e5ef81e7b3eab489094a9ad33bd
oleksandr.qa <serhadov.o@wezom.com.ua>
2023-10-13 15:44:43 +0300

    delete trash hardcode

--- src/test/java/elements/HomeElements.java
+++ src/test/java/elements/HomeElements.java
@@ -9,72 +9,48 @@ public class HomeElements extends AbstractBasePage {
         super(driver);
     }
 
-    private final static String HEADER_REGISTRATION_AUTHORIZATION_BTN = "//div[@class='header__actions-button']";
-    private final static String REGISTRATION_AUTHORIZATION_POPUP_TEXT = "(//div[@class='h3 _text-center _mb-xl'])[1]";
-    private final static String REGISTRATION_POPUP_BTN = "//div[contains(@class, 'popup__aside')]/button";
-    private final static String EMAIL_OR_NUMBER_POPUP_FIELD = "//div[@class='form-control']/input[@name='login']";
-    private final static String PASSWORD_POPUP_FIELD = "//div[@class='form-control _mt-xs']/input[@name='password']";
-    private final static String FIRST_NAME_FIELD = "//input[@name='name']";
-    private final static String LAST_NAME_FIELD = "//input[@name='surname']";
-    private final static String TELEPHONE_FIELD = "//form[1]/div[4]/input[1][@name='phone']";
-    private final static String EMAIL_FIELD = "//input[@name='email']";
-    private final static String PASSWORD_FIELD = "//input[@name='password']";
-    private final static String PASSWORD_CONFIRMATION_FIELD = "//div[@class='popup__view']//form//div[7]/input";
-    private final static String TERMS_OF_USE_CHECKBOX = "//label[@class='input-choice']/span[1]";
-    private final static String SUBMIT_REGISTRATION_BTN = "//button[@class='button button_full button_one']";
-    private final static String PRODUCT_CATALOGUE_MENU = "//div[@class='menu__button ']";
-    private final static String HOUSEHOLD_APPLIANCES = "//li/a[@href='https://evrika.wezom.agency/catalog/bytovaya-tehnika/c244']";
-    private final static String FANS_CATEGORY = "//ul[@class='menu__sub-list _ul-like']//a[@href='https://evrika.wezom.agency/catalog/ventilyatory/c266']";
-    private final static String X_CLOSE_POPUP_BTN = "//div/button[@class='modal-close']";
-    private final static String BASKET = "//div[@class='header__cart-badge _flex _justify-center _items-center']";
-    private final static String SUBMIT_ORDER = "//a[@class='button button_one button_full button_size_sm _mb-sm']";
-    private final static String BASKET_POPUP_TEXT = "//div[contains(@class, 'cart__title h3')]";
-    private final static String BASKET_POPUP_CART_BODY = "//div[contains(@class, 'cart__body')]";
-    private final static String BASKET_POPUP_SUBMIT_ORDER_BTN = "//a[@class='button button_one']";
-    private final static String BASKET_POPUP_TOTAL_COST_VALUE = "//div[@class='cart-resume__cost-value']";
-
     public WebElement getRegistrationAuthorizationBtn() {
-        return waitClickableElementByXpath(HEADER_REGISTRATION_AUTHORIZATION_BTN);
+        return waitClickableElementByXpath("//div[@class='header__actions-button']");
     }
 
     public WebElement getRegistrationAuthorizationPopUpText() {
-        return waitVisibleOfElement(REGISTRATION_AUTHORIZATION_POPUP_TEXT);
+        return waitVisibleOfElement("(//div[@class='h3 _text-center _mb-xl'])[1]");
     }
 
     public WebElement getFirstNameField() {
-        return waitClickableElementByXpath(FIRST_NAME_FIELD);
+        return waitClickableElementByXpath("//input[@name='name']");
     }
 
     public WebElement getPasswordPopUpField() {
-        return waitVisibleOfElement(PASSWORD_POPUP_FIELD);
+        return waitVisibleOfElement("//div[@class='form-control _mt-xs']/input[@name='password']");
     }
 
     public WebElement getEmailOrNumberPopUpField() {
-        return waitVisibleOfElement(EMAIL_OR_NUMBER_POPUP_FIELD);
+        return waitVisibleOfElement("//div[@class='form-control']/input[@name='login']");
     }
 
     public WebElement getLastNameField() {
-        return waitClickableElementByXpath(LAST_NAME_FIELD);
+        return waitClickableElementByXpath("//input[@name='surname']");
     }
 
     public WebElement getTelephoneField() {
-        return waitClickableElementByXpath(TELEPHONE_FIELD);
+        return waitClickableElementByXpath("//form[1]/div[4]/input[1][@name='phone']");
     }
 
     public WebElement getEmailField() {
-        return waitClickableElementByXpath(EMAIL_FIELD);
+        return waitClickableElementByXpath("//input[@name='email']");
     }
 
     public WebElement getPasswordField() {
-        return waitClickableElementByXpath(PASSWORD_FIELD);
+        return waitClickableElementByXpath("//input[@name='password']");
     }
 
     public WebElement getPasswordConfirmationField() {
-        return waitClickableElementByXpath(PASSWORD_CONFIRMATION_FIELD);
+        return waitClickableElementByXpath("//div[@class='popup__view']//form//div[7]/input");
     }
 
     public WebElement getRegistrationPopUpBtn() {
-        return waitClickableElementByXpath(REGISTRATION_POPUP_BTN);
+        return waitClickableElementByXpath("//div[contains(@class, 'popup__aside')]/button");
     }
 
     public WebElement getCurrentPurchaseBasketBtn(String supply) {
@@ -82,46 +58,47 @@ public class HomeElements extends AbstractBasePage {
     }
 
     public WebElement getIAgreeCheckBox() {
-        return waitClickableElementByXpath(TERMS_OF_USE_CHECKBOX);
+        return waitClickableElementByXpath("//label[@class='input-choice']/span[1]");
     }
 
     public WebElement getSubmitRegistrationBtn() {
-        return waitClickableElementByXpath(SUBMIT_REGISTRATION_BTN);
+        return waitClickableElementByXpath("//button[@class='button button_full button_one']");
     }
 
     public WebElement getProductCatalogueMenu() {
-        return waitVisibleOfElement(PRODUCT_CATALOGUE_MENU);
+        return waitVisibleOfElement("//div[@class='menu__button ']");
     }
 
     public WebElement getHouseHoldAppliances() {
-        return waitVisibleOfElement(HOUSEHOLD_APPLIANCES);
+        return waitVisibleOfElement("//li/a[@href='https://evrika.wezom.agency/catalog/bytovaya-tehnika/c244']");
     }
 
     public WebElement getFansProduct() {
-        return waitVisibleOfElement(FANS_CATEGORY);
+        return waitVisibleOfElement("//ul[@class='menu__sub-list _ul-like']//a[@href='https://evrika.wezom.agency/catalog/ventilyatory/c266']");
     }
 
     public WebElement getXClosePopUpBtn() {
-        return waitClickableElementByXpath(X_CLOSE_POPUP_BTN);
+        return waitClickableElementByXpath("//div/button[@class='modal-close']");
     }
 
     public WebElement getBasketBtn() {
-        return waitClickableElementByXpath(BASKET);
+        return waitClickableElementByXpath("//div[@class='header__cart-badge _flex _justify-center _items-center']");
     }
 
     public WebElement getSubmitOrderBtn() {
-        return waitClickableElementByXpath(SUBMIT_ORDER);
+        return waitClickableElementByXpath("//a[@class='button button_one button_full button_size_sm _mb-sm']");
     }
 
     public WebElement getBasketPopUpText() {
-        return waitVisibleOfElement(BASKET_POPUP_TEXT);
+        return waitVisibleOfElement("//div[contains(@class, 'cart__title h3')]");
     }
+
     public WebElement getBasketPopUpCartBody() {
-        return waitVisibleOfElement(BASKET_POPUP_CART_BODY);
+        return waitVisibleOfElement("//div[contains(@class, 'cart__body')]");
     }
 
     public WebElement getBasketPopUpTotalCostValue() {
-        return waitVisibleOfElement(BASKET_POPUP_TOTAL_COST_VALUE);
+        return waitVisibleOfElement("//div[@class='cart-resume__cost-value']");
     }
 
 
